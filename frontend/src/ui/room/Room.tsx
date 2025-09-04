73e0d76f89c647a8bf849949cde690efa4e155b1
elhalj <wilsonikedakoffi7@gmail.com>
2025-09-04 23:39:17 +0000

    ajust room ui task in roomId

--- frontend/src/ui/room/Room.tsx
+++ frontend/src/ui/room/Room.tsx
@@ -108,7 +108,7 @@ const RoomUI = () => {
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
-      {loading && <Loader/>}
+      {loading && <Loader />}
       <RoomRow />
     </div>
   );
